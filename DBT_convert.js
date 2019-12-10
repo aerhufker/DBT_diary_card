d1596f09774c4980bf6f824acf8b0803cca80395
Mike Xiao <mikemxxiao@gmail.com>
2019-12-10 18:33:50 -0500

    change branch name in URI

--- DBT_convert.js
+++ DBT_convert.js
@@ -6,7 +6,7 @@ const protocolName = "DBT_diary_card"
 //2. create your raw github repo URL
 const userName = 'hotavocado'
 const repoName = 'DBT_diary_card'
-const branchName = 'clone1'
+const branchName = 'master'
 
 let yourRepoURL = `https://raw.githubusercontent.com/${userName}/${repoName}/${branchName}`
 
